c6c3e74702b164a9abb65bad1805756eeb91c2fc
devcontainerDuy <khanhduytran1803@gmail.com>
2025-03-13 09:25:56 +0700

    Refactor project to use Inertia.js with React, update routes, and add Laravel File Manager integration

---
+++ resources/views/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CMS - CodeAcademyVN</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+
+    <!-- Styles / Scripts -->
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
+    @endif
+
+    @inertiaHead
+</head>
+
+<body>
+    @inertia
+</body>
+
+</html>
